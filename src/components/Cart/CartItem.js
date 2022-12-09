5e625827f1fb04db1cb1c0b2672915727d42b54f
David <dmontanez@aceproject.space>
2022-12-09 14:34:20 -0600

    Configure items to display on cart and styled the cart list.

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,10 @@
 import classes from './CartItem.module.css';
 
+/**
+ * Returns the Cart Item component.
+ * 
+ * @returns The Cart Item component.
+ */
 const CartItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -7,11 +12,13 @@ const CartItem = (props) => {
     <li className={classes['cart-item']}>
       <div>
         <h2>{props.name}</h2>
+
         <div className={classes.summary}>
           <span className={classes.price}>{price}</span>
           <span className={classes.amount}>x {props.amount}</span>
         </div>
       </div>
+
       <div className={classes.actions}>
         <button onClick={props.onRemove}>−</button>
         <button onClick={props.onAdd}>+</button>
