e241e01bac036244fd8bf6147061316a2a8d22a1
David <dmontanez@aceproject.space>
2022-12-12 15:57:11 -0600

    Configured cart UI visual bump to signal added items to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -10,17 +10,38 @@ import classes from "./HeaderCartButton.module.css";
  * @returns The Header Cart Button component.
  */
 const HeaderCartButton = (props) => {
-    const cartCtx = useContext(CartContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
 
     return (
         <>
             <button
                 onClick={props.onClick}
-                className={classes.button}
+                className={btnClasses}
             >
                 <span className={classes.icon}>
                     <CartIcon />
