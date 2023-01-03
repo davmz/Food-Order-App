45c7b1b77a0c8cebd1abf0596206147881373e32
David <dmontanez@aceproject.space>
2023-01-02 22:20:54 -0600

    Configured all of the dibbymeals unto Google Firebase using Realtime Database making HTTP request to retrieve and populate the menu

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
+
 import MealItem from "./MealItem/MealItem.js";
 
 import Card from "../UI/Card.js";
-import { DUMMY_MEALS } from "./dummy-meals.js";
+// import { DUMMY_MEALS } from "./dummy-meals.js";
 import classes from "./AvailableMeals.module.css";
 
 /**
@@ -10,7 +12,32 @@ import classes from "./AvailableMeals.module.css";
  * @returns The Available Meals component.
  */
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async() => {
+            const reponse = await fetch("https://udemy-react-http-7113e-default-rtdb.firebaseio.com/meals.json");
+            const responseData = await reponse.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                    src: responseData[key].src
+                })
+            }
+
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals();
+    }, []);
+
+    const mealsList = meals.map((meal) => (
         <MealItem
             id={meal.id}
             key={meal.id}
