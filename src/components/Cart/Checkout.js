5361a26092bc8c63ad7204d29681c169885c1797
David <dmontanez@aceproject.space>
2023-01-03 07:49:14 -0600

    Initialize Checkout components for checkout form - additional feature to checkout

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,16 @@
+import classes from "./Checkout.module.css";
+
+/**
+ * Returns the Cart Checkout component.
+ * 
+ * @returns The Cart Checkout component.
+ */
+const Checkout = () => {
+    return (
+        <>
+            
+        </>
+    );
+}
+ 
+export default Checkout;
